2e184a638692a31f5ad08e81e8217b2f07ee5118
Dylan <dylan.reeder5@gmail.com>
2022-03-28 19:15:32 -0400

    WIP: error with .razor.g.cs file, base models and view

---
+++ assignments/DylanReedGroceryApp/DylanReedGroceryApp/Models/FoodItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace DylanReedGroceryApp
+{
+    public class FoodItem
+    {
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+        public int Calories { get; set; }
+        public string Name { get; set; }
+        public FoodItem()
+        {
+        }
+    }
+}
---
+++ assignments/DylanReedGroceryApp/DylanReedGroceryApp/Models/GroceryIsle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DylanReedGroceryApp.Models
+{
+    public class GroceryIsle
+    {
+        public List<FoodItem> FoodItems { get; set; }
+        public string IsleName { get; set; }
+        public Guid IsleNumber { get; set; }
+        public GroceryIsle()
+        {
+        }
+    }
+}
